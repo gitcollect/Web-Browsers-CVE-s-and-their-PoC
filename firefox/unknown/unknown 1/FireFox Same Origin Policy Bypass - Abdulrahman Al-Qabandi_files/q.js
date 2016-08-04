d6d59fe9529ffe9d5ddb5b86b2c6a8debe412bd3
ms95 <mohammad.siav@hotmail.com>
2016-08-05 00:31:23 +0430

    some poc added

---
+++ firefox/unknown/unknown 1/FireFox Same Origin Policy Bypass - Abdulrahman Al-Qabandi_files/q.js
@@ -0,0 +1,8 @@
+	function randomIntFromInterval(min,max)
+{//stackoverflow#4959975<3
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+		
+if(navigator.userAgent.indexOf("Chrome") != -1 ){//Chrome
+				
+}
